bd9a1d23d75e5c4a6b515bb95f32d2a70577f24f
0shirase <lucas0999.lb@gmail.com>
2025-04-23 14:24:48 +0200

    feat: ajout du data-id  et de la pop-up de refus d'ajout du même produit

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".add-basket-button");
-  const basket = document.getElementById("basket");
+  const basketContain = document.getElementById("basket");
+  let idCounter = 0;
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const product = button.parentElement;
-      const cloneProduct = product.cloneNode(true);
+      const productName = product.querySelector("p")?.textContent;
+
+      const existingBasket = [...basketContain.children].some((item) => {
+        /* sert à transformer la liste de produits en tableau*/
+        const name =
+          item.querySelector(
+            "p"
+          )?.textContent; /* sert à sélectionner le content du premier P pour voir si il est déjà existant*/
+        return name === productName; /* retourne la comparaison */
+      });
+
+      if (existingBasket) {
+        alert("Vous avez déjà selectionné ce produit");
+      } else {
+        const clonedProduct = product.cloneNode(true);
+        const clonedButton = clonedProduct.querySelector(".add-basket-button");
+        if (clonedButton) {
+          clonedButton.remove();
+        }
+
+        clonedProduct.setAttribute("data-id", idCounter);
+        idCounter++;
+        basketContain.appendChild(clonedProduct);
+      }
     });
   });
 });
